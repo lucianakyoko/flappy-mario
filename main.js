859fdfe21cbabc4d82b9ca9d24d3d8cb77e306a3
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-07-14 16:15:11 -0300

    feat(main, mario.js): add mario animation

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 import { Mario } from "./assets/scripts/mario";
 
-console.log('[LUCIANA KYOKO] - Visite o meu portfolio https://lucianakyoko.com');
-
 const mario = new Mario;
+
+export let frames = 0;
 
 const animate = () => {
   mario.draw();
+
+  frames = frames + 1;
   requestAnimationFrame(animate);
 };
 
 animate();
+console.log('[LUCIANA KYOKO] - Visite o meu portfolio https://lucianakyoko.com');
