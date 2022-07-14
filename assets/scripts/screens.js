0af9072ec69b5e2466149b140e964ad4b6055b67
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-07-14 18:42:47 -0300

    feat(assets/scripts): create startMessage.js

--- assets/scripts/screens.js
+++ assets/scripts/screens.js
@@ -1,11 +1,14 @@
 import { Background } from "./background";
+import { changeScreen } from "./changeScreen";
 import { Ground } from "./ground";
 import { Mario } from "./mario";
+import { StartMessage } from "./startMessage";
 
 export let mario = new Mario();
 export let ground = new Ground();
 
 const background = new Background();
+const startMessage = new StartMessage();
 
 
 export const Screens = {
@@ -16,12 +19,32 @@ export const Screens = {
     },
     draw() {
       background.draw();
+      startMessage.draw();
       mario.draw();
       ground.draw();
     },
     update() {
       ground.update();
+    },
+    click() {
+      changeScreen(Screens.GAME);
     }
+  },
+
+  GAME: {
+    initialize() {
+
+    },
+    draw() {
+      background.draw();
+      mario.draw();
+      ground.draw();
+    },
+    update() {
+      ground.update();
+      // mario.update();
+    },
+    click() {},
   }
 };
 
