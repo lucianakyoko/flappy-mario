f9e4edca55f7b1130860c7916e29882d47b1168a
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-07-14 20:08:39 -0300

    feat(assets/scripts): create soundEffects.js

---
+++ assets/scripts/soundEffects.js
@@ -0,0 +1,8 @@
+export const lostLife = new Audio();
+lostLife.src = './assets/sounds/lost_life.wav';
+
+export const flySound = new Audio();
+flySound.src = './assets/sounds/fly.wav';
+
+export const gameThemeSound = new Audio();
+gameThemeSound.src = './assets/sounds/theme.wav';
